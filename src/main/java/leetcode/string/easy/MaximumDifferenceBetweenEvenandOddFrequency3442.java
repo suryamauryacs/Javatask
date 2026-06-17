914c025015eea7305370069e8645f28e9df6bb83
surya maurya <suryamauryacs@gmail.com>
2026-06-17 18:13:41 +0530

    Leet 3442 Optimal approach

--- src/main/java/leetcode/string/easy/MaximumDifferenceBetweenEvenandOddFrequency3442.java
+++ src/main/java/leetcode/string/easy/MaximumDifferenceBetweenEvenandOddFrequency3442.java
@@ -59,6 +59,30 @@ public class MaximumDifferenceBetweenEvenandOddFrequency3442 {
         String s = sc.nextLine();
 
         System.out.println(maxDifference(s));
+        System.out.println(maxDifferenceOptimal(s));
+    }
+
+    public static int maxDifferenceOptimal(String s){
+        int[] freq = new int[26];
+
+        for(char c : s.toCharArray()){
+            freq[c - 'a']++;
+        }
+
+        int maxOdd = Integer.MIN_VALUE;
+        int minEven = Integer.MAX_VALUE;
+
+        for(int f : freq){
+            if ( f == 0)
+                continue;
+            if(f % 2 == 1){
+                maxOdd = Math.max(maxOdd, f);
+            }else{
+                minEven = Math.min(minEven, f);
+            }
+        }
+
+        return maxOdd - minEven;
     }
 
     public static int maxDifference(String s){
