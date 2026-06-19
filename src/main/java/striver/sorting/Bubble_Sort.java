5cccb7eff5b610bf5f0992c1207c0d5751b062e2
surya maurya <suryamauryacs@gmail.com>
2026-06-19 17:18:44 +0530

    Bubble Sort

--- src/main/java/striver/sorting/Bubble_Sort.java
+++ src/main/java/striver/sorting/Bubble_Sort.java
@@ -20,12 +20,18 @@ public class Bubble_Sort {
     public static int[] bubble_Sort(int[] arr){
         int n = arr.length;
         for(int i = n-1; i>=1;i--){
+            int didswap = 0;
             for(int j = 0;j<=i-1;j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didswap = 1;
                 }
+            }
+
+            if(didswap == 0){
+                break;
             }
         }
 
