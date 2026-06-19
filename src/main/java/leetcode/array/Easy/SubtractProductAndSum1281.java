cbc65a1409ffd1eff047074b85e551d79ef41506
surya maurya <suryamauryacs@gmail.com>
2026-06-19 22:51:15 +0530

    Leet 1281

--- src/main/java/leetcode/array/Easy/SubtractProductAndSum1281.java
+++ src/main/java/leetcode/array/Easy/SubtractProductAndSum1281.java
@@ -1,8 +1,63 @@
 package leetcode.array.Easy;
+//https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
+
+//1281. Subtract the Product and Sum of Digits of an Integer
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+//
+//
+//Example 1:
+//
+//Input: n = 234
+//Output: 15
+//Explanation:
+//Product of digits = 2 * 3 * 4 = 24
+//Sum of digits = 2 + 3 + 4 = 9
+//Result = 24 - 9 = 15
+//Example 2:
+//
+//Input: n = 4421
+//Output: 21
+//Explanation:
+//Product of digits = 4 * 4 * 2 * 1 = 32
+//Sum of digits = 4 + 4 + 2 + 1 = 11
+//Result = 32 - 11 = 21
+//
+//
+//Constraints:
+//
+//1 <= n <= 10^5
+//
+
+import java.util.Scanner;
 
 public class SubtractProductAndSum1281 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+
+        System.out.println(subtractProductAndSum(num));
+
+
+    }
+
+    public static  int subtractProductAndSum(int n) {
+        int digit = 0, pd = 1, sd = 0;
+        while(n > 0){
+            digit = n %10;
+            pd = pd*digit;
+            sd = sd+digit;
+            n = n/10;
+        }
+
+        return pd-sd;
     }
 
 
