b3aa9daf29cee41619c662e9f781e88cfeecdb9b
surya maurya <suryamauryacs@gmail.com>
2026-06-22 18:50:22 +0530

    Way to break singleton

--- src/main/java/com/designpattern/day1/Example.java
+++ src/main/java/com/designpattern/day1/Example.java
@@ -1,15 +1,76 @@
 package com.designpattern.day1;
 
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Example {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException,CloneNotSupportedException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
+
+//        Samosa samosa = Samosa.getSomosa();
+//
+//        System.out.println(samosa.hashCode());
+//
+//        Samosa samosa2 = Samosa.getSomosa();
+//
+//        System.out.println(samosa2.hashCode());
+//
+//
+//        System.out.println(Jalebi.getJalebi().hashCode());
+//        System.out.println(Jalebi.getJalebi().hashCode());
+
+
+        /*
+
+        ₹1. Reflection API to break singleton pattern
+        solution : 1 -> if object is there ==> throw exeption from inside contructor
+        2-> use enum
+
+        2.-> Deserialization :
+        solution : implement readResolve method
+
+
+       3. cloning
+
+
+         */
+
+//        Samosa s1 = Samosa.getSomosa();
+//        System.out.println(s1.hashCode());
+//
+//        Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Samosa s2 = constructor.newInstance();
+//        System.out.println(s2.hashCode());
+
+
+//        Samosa s1 = Samosa.INSTANCE;
+//        System.out.println(s1.hashCode());
+//        s1.test();
+//        Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Samosa s2 = constructor.newInstance();
+//        System.out.println(s2.hashCode());
+
+
+//        Samosa samosa = Samosa.getSomosa();
+//        System.out.println(samosa.hashCode());
+//        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("abc.ob"));
+//        oos.writeObject(samosa);
+//
+//        System.out.println("serialization done..");
+//
+//        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("abc.ob"));
+//        Samosa s2 = (Samosa) ois.readObject();
+//        System.out.println(s2.hashCode());
+//
+
 
         Samosa samosa = Samosa.getSomosa();
-
         System.out.println(samosa.hashCode());
+        Samosa s3 = (Samosa) samosa.clone();
+        System.out.println(s3.hashCode());
 
-        Samosa samosa2 = Samosa.getSomosa();
-
-        System.out.println(samosa2.hashCode());
     }
 }
--- src/main/java/com/designpattern/day1/Samosa.java
+++ src/main/java/com/designpattern/day1/Samosa.java
@@ -1,6 +1,17 @@
 package com.designpattern.day1;
 
-public class Samosa {
+//public enum Samosa{
+//    INSTANCE;
+//
+//    public void test(){
+//        System.out.println("Test method");
+//    }
+//}
+
+import java.io.Serializable;
+
+public class Samosa implements Serializable, Cloneable {
+
 
     private static Samosa samosa;
 
@@ -9,17 +20,47 @@ public class Samosa {
 
 
     private Samosa(){
-
+//        if(samosa != null){
+//            throw new RuntimeException("you are trying to break single pattern");
+//        }
     }
 
-    public static Samosa getSomosa(){
+//    //Lazy way of Creating single object
+//    public static Samosa getSomosa(){
+//        // object of this class
+//        if(samosa == null){
+//            samosa = new Samosa();
+//        }
+//
+//        return samosa;
+//    }
+//
+
+    //Lazy way of Creating single object
+    public  static Samosa getSomosa(){
         // object of this class
         if(samosa == null){
-            samosa = new Samosa();
+            synchronized (Samosa.class){
+                if(samosa == null){
+                    samosa = new Samosa();
+
+                }
+            }
         }
 
         return samosa;
     }
+
+
+    public Object readResolve(){
+        return samosa;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
+
 
 }
 
