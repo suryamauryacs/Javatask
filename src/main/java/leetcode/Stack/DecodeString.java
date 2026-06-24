6ae00878d5bdee10707341a82bc3dd489feb6c49
surya maurya <suryamauryacs@gmail.com>
2026-06-24 18:16:49 +0530

    Stack

---
+++ src/main/java/leetcode/Stack/DecodeString.java
@@ -0,0 +1,32 @@
+package leetcode.Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DecodeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the String : ");
+        String s = sc.nextLine();
+
+        System.out.println(decodeString(s));
+
+
+    }
+
+    public static String decodeString(String s){
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+
+        int k = 0;
+        StringBuilder current = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                k = k + 10 + (c - '0');
+
+            }
+        }
+
+    }
+}
