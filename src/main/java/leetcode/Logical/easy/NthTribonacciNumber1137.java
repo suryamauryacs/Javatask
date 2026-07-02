03083cdd4fcddbb0752c4f23433809e5c12a5bee
surya maurya <suryamauryacs@gmail.com>
2026-07-02 14:23:08 +0530

    Leetcode 1137

---
+++ src/main/java/leetcode/Logical/easy/NthTribonacciNumber1137.java
@@ -0,0 +1,7 @@
+package leetcode.Logical.easy;
+//https://leetcode.com/problems/n-th-tribonacci-number/description/
+public class NthTribonacciNumber1137 {
+    public static void main(String[] args) {
+
+    }
+}
