70eca4ec916f39cd386b33bacb34d5690bef18c8
surya maurya <suryamauryacs@gmail.com>
2026-07-03 22:48:22 +0530

    Strinbuilder as stack[C using approach

--- src/main/java/leetcode/string/easy/RemoveAllAdjacentDuplicatesInString1047.java
+++ src/main/java/leetcode/string/easy/RemoveAllAdjacentDuplicatesInString1047.java
@@ -45,6 +45,8 @@ public class RemoveAllAdjacentDuplicatesInString1047 {
 
         System.out.println(removeDuplicates(input));
 
+        System.out.println(removeDuplicate(input));
+
     }
 
     public static String removeDuplicates(String s){
@@ -62,6 +64,24 @@ public class RemoveAllAdjacentDuplicatesInString1047 {
 
         for(char ch : stack){
             sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+
+
+
+    public static String removeDuplicate(String s){
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            int len = sb.length();
+
+            if(len > 0 && sb.charAt(len-1) == ch){
+                sb.deleteCharAt(len-1);
+            }else{
+                sb.append(ch);
+            }
         }
 
         return sb.toString();
