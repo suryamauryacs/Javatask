e872108308a4a759c15c6f5532fedc5ccad7d763
surya maurya <suryamauryacs@gmail.com>
2026-07-06 00:08:17 +0530

    Leet 1137

---
+++ src/main/java/leetcode/Logical/easy/NthTribonacciNumbers1137.java
@@ -0,0 +1,52 @@
+package leetcode.Logical.easy;
+////package leetcode.Logical.easy;
+////https://leetcode.com/problems/n-th-tribonacci-number/description/
+//
+
+import java.util.Scanner;
+
+/// / 1137. N-th Tribonacci Number
+/// /Easy
+/// /Topics
+/// /premium lock icon
+/// /Companies
+/// /Hint
+/// /The Tribonacci sequence Tn is defined as follows:
+/// /
+/// /T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+/// /
+/// /Given n, return the value of Tn.
+/// /
+/// /
+/// /
+/// /Example 1:
+/// /
+/// /Input: n = 4
+/// /Output: 4
+/// /Explanation:
+/// /T_3 = 0 + 1 + 1 = 2
+/// /T_4 = 1 + 1 + 2 = 4
+/// /Example 2:
+/// /
+/// /Input: n = 25
+/// /Output: 1389537
+/// /
+/// /
+/// /Constraints:
+/// /
+/// /0 <= n <= 37
+/// /The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+/// /public class NthTribonacciNumber1137 {
+/// /    public static void main(String[] args) {
+/// /
+/// /    }
+/// /}
+public class NthTribonacciNumbers1137 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int n = sc.nextInt();
+
+
+    }
+}
