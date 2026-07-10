8015afded01e0436a27b6466d8d8d4f700230f6e
surya maurya <suryamauryacs@gmail.com>
2026-07-10 23:04:48 +0530

    Leet 1518

---
+++ src/main/java/leetcode/challengelogical/WaterBottles1518.java
@@ -0,0 +1,14 @@
+package leetcode.challengelogical;
+//https://leetcode.com/problems/water-bottles/description/
+
+import java.util.Scanner;
+
+public class WaterBottles1518 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number: umBottles = 9, numExchange = 3");
+        String input = sc.nextLine();
+
+    }
+}
