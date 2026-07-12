3992bebcc21501742d490332fbed690fcdf306a4
surya maurya <suryamauryacs@gmail.com>
2026-07-12 23:39:06 +0530

    Leet 989

--- src/main/java/leetcode/Logical/EasyProblme/AddtoArrayFormofInteger989.java
+++ src/main/java/leetcode/Logical/EasyProblme/AddtoArrayFormofInteger989.java
@@ -1,4 +1,6 @@
 package leetcode.Logical.EasyProblme;
+//https://leetcode.com/problems/add-to-array-form-of-integer/description/
+
 
 public class AddtoArrayFormofInteger989 {
     public static void main(String[] args) {
