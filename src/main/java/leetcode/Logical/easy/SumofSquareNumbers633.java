b5bb867fd0a6e38db94fffaac126f79aec496cdd
surya maurya <suryamauryacs@gmail.com>
2026-07-16 14:31:34 +0530

    Update

--- src/main/java/leetcode/Logical/easy/SumofSquareNumbers633.java
+++ src/main/java/leetcode/Logical/easy/SumofSquareNumbers633.java
@@ -1,5 +1,23 @@
 package leetcode.Logical.easy;
 //https://leetcode.com/problems/sum-of-square-numbers/description/
+//Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+//
+//
+//
+//Example 1:
+//
+//Input: c = 5
+//Output: true
+//Explanation: 1 * 1 + 2 * 2 = 5
+//Example 2:
+//
+//Input: c = 3
+//Output: false
+//
+//
+//Constraints:
+//
+//0 <= c <= 231 - 1
 
 import java.util.Scanner;
 
