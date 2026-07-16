28f1e9ab456cb18c17c12151a4d89686141af47c
surya maurya <suryamauryacs@gmail.com>
2026-07-16 14:29:11 +0530

    Leet 633

---
+++ src/main/java/leetcode/Logical/easy/SumofSquareNumbers633.java
@@ -0,0 +1,8 @@
+package leetcode.Logical.easy;
+//https://leetcode.com/problems/sum-of-square-numbers/description/
+
+public class SumofSquareNumbers633 {
+    public static void main(String[] args) {
+
+    }
+}
