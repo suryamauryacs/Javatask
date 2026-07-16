90ead90a32614ea0257c968a4b561a6f8f8f1b47
surya maurya <suryamauryacs@gmail.com>
2026-07-16 14:30:25 +0530

    Update

--- src/main/java/leetcode/Logical/easy/SumofSquareNumbers633.java
+++ src/main/java/leetcode/Logical/easy/SumofSquareNumbers633.java
@@ -1,8 +1,14 @@
 package leetcode.Logical.easy;
 //https://leetcode.com/problems/sum-of-square-numbers/description/
 
+import java.util.Scanner;
+
 public class SumofSquareNumbers633 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number ; ");
+        int n = sc.nextInt();
+
 
     }
 }
