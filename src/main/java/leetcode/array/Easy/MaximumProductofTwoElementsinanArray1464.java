a95be5fe551df0bcf92ab27df94812be57f19f00
surya maurya <suryamauryacs@gmail.com>
2026-07-17 14:20:58 +0530

    Leet 1464

---
+++ src/main/java/leetcode/array/Easy/MaximumProductofTwoElementsinanArray1464.java
@@ -0,0 +1,11 @@
+package leetcode.array.Easy;
+
+import java.util.Scanner;
+
+//https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/
+public class MaximumProductofTwoElementsinanArray1464 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
