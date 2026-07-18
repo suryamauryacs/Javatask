72defe93189edcec1544c8b0e0fee08c1a34e9e1
surya maurya <suryamauryacs@gmail.com>
2026-07-18 23:20:31 +0530

    input sorting

--- src/main/java/striveplaylist/array/Day2/Array/CheckSorting.java
+++ src/main/java/striveplaylist/array/Day2/Array/CheckSorting.java
@@ -1,5 +1,6 @@
 package striveplaylist.array.Day2.Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CheckSorting {
@@ -9,5 +10,22 @@ public class CheckSorting {
         String input = sc.nextLine();
 
         int[] arr = strToArr(input);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] strToArr(String s){
+        if(s == null)
+            return new int[0];
+
+//        s = s.replaceAll("[\\[\\]<>[\\<>()\\]","");
+
+//        s = s.replaceAll("[\\[\\]<>\\(\\){}]", "");
+        s = s.replaceAll("[\\[\\]<>\\(\\){}]","");
+
+        if(s.isEmpty())
+            return new int[0];
+
+        return Arrays.stream(s.split(",")).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
+
     }
 }
