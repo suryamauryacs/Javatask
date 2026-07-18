11d285e47daa5b5a1b3f2917a1ed0dce97044d36
surya maurya <suryamauryacs@gmail.com>
2026-07-18 23:13:09 +0530

    check sorting array

---
+++ src/main/java/striveplaylist/array/Day2/Array/CheckSorting.java
@@ -0,0 +1,13 @@
+package striveplaylist.array.Day2.Array;
+
+import java.util.Scanner;
+
+public class CheckSorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Array like : [1,2,3]:");
+        String input = sc.nextLine();
+
+        int[] arr = strToArr(input);
+    }
+}
