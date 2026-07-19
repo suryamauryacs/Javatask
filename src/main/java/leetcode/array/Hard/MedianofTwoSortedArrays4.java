8167768a91261a399d3ff3bea77c7ca1c12a20a9
surya maurya <suryamauryacs@gmail.com>
2026-07-19 13:44:24 +0530

    merge two array method

--- src/main/java/leetcode/array/Hard/MedianofTwoSortedArrays4.java
+++ src/main/java/leetcode/array/Hard/MedianofTwoSortedArrays4.java
@@ -36,24 +36,26 @@ public class MedianofTwoSortedArrays4 {
         System.out.println("Enter the num1 = [1,2], num2 = [3,4]:");
         String input = sc.nextLine();
 
-        int[] arr1 = strToArr(input, "num1");
-        int[] arr2 = strToArr(input, "num2");
+        int[] arr1 = strToArr(input, "nums1");
+        int[] arr2 = strToArr(input, "nums2");
 
         System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(findMedianSortedArrays(arr1, arr2));
     }
 
     public static int[] strToArr(String s, String label){
         int numpos = s.indexOf(label);
         int eqpos = s.indexOf("=",numpos);
         int obpos = s.indexOf("[",eqpos);
-        int cbpos = s.indexOf("]");
+        int cbpos = s.indexOf("]",obpos);
 
-        s = s.substring(obpos+1,cbpos);
+        String newStr = s.substring(obpos+1,cbpos);
 
-        return Arrays.stream(s.split(",")).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
+        return Arrays.stream(newStr.split(",")).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
     }
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] ans = merge(nums1, nums2);
         double res = 0.0;
         if(ans.length % 2 == 0){
@@ -67,7 +69,7 @@ public class MedianofTwoSortedArrays4 {
 
     }
 
-    public int[] merge(int[] num1, int[] num2){
+    public static int[] merge(int[] num1, int[] num2){
         int[] ans = new int[num1.length + num2.length];
         int p1= 0;
         int p2= 0;
