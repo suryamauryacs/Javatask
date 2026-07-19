be9c4e87a5a7636525f74ab9ab393b60142756ca
surya maurya <suryamauryacs@gmail.com>
2026-07-19 13:21:23 +0530

    leet 4

--- src/main/java/leetcode/array/Hard/MedianofTwoSortedArrays4.java
+++ src/main/java/leetcode/array/Hard/MedianofTwoSortedArrays4.java
@@ -1,4 +1,28 @@
 package leetcode.array.Hard;
 //https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+//4. Median of Two Sorted Arrays
+//Solved
+//Hard
+//Topics
+//premium lock icon
+//Companies
+//Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+//
+//The overall run time complexity should be O(log (m+n)).
+//
+//
+//
+//Example 1:
+//
+//Input: nums1 = [1,3], nums2 = [2]
+//Output: 2.00000
+//Explanation: merged array = [1,2,3] and median is 2.
+//Example 2:
+//
+//Input: nums1 = [1,2], nums2 = [3,4]
+//Output: 2.50000
+//Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
 public class MedianofTwoSortedArrays4 {
 }
