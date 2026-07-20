8de3ec797285376973f7141d8e19084b540dc1f6
surya maurya <suryamauryacs@gmail.com>
2026-07-20 09:29:35 +0530

    Two pointer appraoch leet4

---
+++ src/main/java/leetcode/array/Hard/FindMedianSortedArrays.java
@@ -0,0 +1,57 @@
+package leetcode.array.Hard;
+
+import java.util.Arrays;
+import java.util.Scanner;
+////https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+////4. Median of Two Sorted Arrays
+////Solved
+////Hard
+////Topics
+////premium lock icon
+////Companies
+////Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+////
+////The overall run time complexity should be O(log (m+n)).
+////
+////
+////
+////Example 1:
+////
+////Input: nums1 = [1,3], nums2 = [2]
+////Output: 2.00000
+////Explanation: merged array = [1,2,3] and median is 2.
+////Example 2:
+////
+////Input: nums1 = [1,2], nums2 = [3,4]
+////Output: 2.50000
+////Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+///
+public class FindMedianSortedArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num1 = [1,2], num2 = [3,4]:");
+        String input = sc.nextLine();
+
+        int[] arr1 = strToArr(input, "nums1");
+        int[] arr2 = strToArr(input, "nums2");
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+//        System.out.println(findMedianSortedArrays(arr1, arr2));
+
+    }
+
+    public static int[] strToArr(String s, String label){
+        int nopos = s.indexOf(label);
+        int eqpos = s.indexOf("=",nopos);
+        int obpos = s.indexOf("[",eqpos);
+        int cbpos = s.indexOf("]",obpos);
+
+        String newStr = s.substring(obpos+1, cbpos);
+        return Arrays.stream(newStr.split(",")).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
+    }
+
+//    public static double findMedianSortedArrays(arr1, arr2){
+//        return null;
+//    }
+}
