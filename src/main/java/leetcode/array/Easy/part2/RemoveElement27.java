d49a0fe78ec8a784c7bc63917a56b868e61c0c0d
surya maurya <suryamauryacs@gmail.com>
2026-07-20 09:58:51 +0530

    Leet 27

---
+++ src/main/java/leetcode/array/Easy/part2/RemoveElement27.java
@@ -0,0 +1,11 @@
+package leetcode.array.Easy.part2;
+
+import java.util.Scanner;
+
+public class RemoveElement27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Array like : [1,2,3]:");
+
+    }
+}
