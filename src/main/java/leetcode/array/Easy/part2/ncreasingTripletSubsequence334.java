48233fcae9ba9bfd56f8dfe9aaf2c8d27cca5261
surya maurya <suryamauryacs@gmail.com>
2026-07-21 23:05:06 +0530

    Leet334

--- src/main/java/leetcode/array/Easy/part2/ncreasingTripletSubsequence334.java
+++ src/main/java/leetcode/array/Easy/part2/ncreasingTripletSubsequence334.java
@@ -1,5 +1,52 @@
 package leetcode.array.Easy.part2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+//https://leetcode.com/problems/increasing-triplet-subsequence/description/
+//iven an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,2,3,4,5]
+//Output: true
+//Explanation: Any triplet where i < j < k is valid.
+//Example 2:
+//
+//Input: nums = [5,4,3,2,1]
+//Output: false
+//Explanation: No triplet exists.
+//Example 3:
+//
+//Input: nums = [2,1,5,0,4,6]
+//Output: true
+//Explanation: One of the valid triplet is (1, 4, 5), because nums[1] == 1 < nums[4] == 4 < nums[5] == 6.
+//
+
+
 public class ncreasingTripletSubsequence334 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Array like [1,2,3]");
+        String input = sc.nextLine();
+        int[] arr = strToArr(input);
+        System.out.println(Arrays.toString(productExceptSelf(arr)));
+    }
+
+
+    public static int[] strToArr(String s){
+        if(s == null)
+            return new int[0];
+        s = s.replaceAll("[\\[\\]<>\\(\\)\\{\\}]","").trim();
+
+        if(s.isEmpty())
+            return new int[0];
+
+        return Arrays.stream(s.split(",")).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
+    }
 
 }
