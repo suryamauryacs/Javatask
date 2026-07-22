90ab11cf85f801ee6223b1c3ab38742049adee02
surya maurya <suryamauryacs@gmail.com>
2026-07-22 12:54:33 +0530

    Leet1800

--- src/main/java/leetcode/array/Easy/part2/MaximumAscendingSubarraySum1800.java
+++ src/main/java/leetcode/array/Easy/part2/MaximumAscendingSubarraySum1800.java
@@ -1,6 +1,18 @@
 package leetcode.array.Easy.part2;
 
+import java.util.Scanner;
+
+//https://leetcode.com/problems/maximum-ascending-subarray-sum/description/
 public class MaximumAscendingSubarraySum1800 {
-    psvm
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Array like [1,2,3]");
+        String input = sc.nextLine();
+        int[] arr = strToArr(input);
+        System.out.println(dominantIndex(arr));
+
+    }
+
 
 }
