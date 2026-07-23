7a6f0e349b53c7c20850c6f7dc022aa928bd9859
surya maurya <suryamauryacs@gmail.com>
2026-07-23 11:14:55 +0530

    Leet704

--- src/main/java/leetcode/array/Easy/part2/FindLuckyIntegerinanArray1394.java
+++ src/main/java/leetcode/array/Easy/part2/FindLuckyIntegerinanArray1394.java
@@ -23,12 +23,31 @@ package leetcode.array.Easy.part2;
 //Explanation: There are no lucky numbers in the array.
 //
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class FindLuckyIntegerinanArray1394 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the Array like [1,2,3]");
+        String input = sc.nextLine();
+        int[] arr = strToArr(input);
+        System.out.println(findLucky(arr));
     }
+
+    public static int[] strToArr(String s){
+        if(s == null)
+            return new int[0];
+        s = s.replaceAll("[\\[\\]<>\\(\\)\\{\\}]","").trim();
+
+        if(s.isEmpty())
+            return new int[0];
+
+        return Arrays.stream(s.split(",")).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
+    }
+
 
     public static int findLucky(int[] arr) {
         HashMap<Integer, Integer> hm = new HashMap<>();
