7e88468e506bdf9b2c714e7a9408dfa62457bbad
surya maurya <suryamauryacs@gmail.com>
2026-07-25 01:40:27 +0530

    Leet 162

---
+++ src/main/java/leetcode/Searching/BinarySearch/FindPeakElement162.java
@@ -0,0 +1,9 @@
+package leetcode.Searching.BinarySearch;
+
+import java.util.Scanner;
+
+public class FindPeakElement162 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner()
+    }
+}
