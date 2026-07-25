464e026784644da3d06df5454302e56928960444
surya maurya <suryamauryacs@gmail.com>
2026-07-25 17:50:27 +0530

    Leet 540

--- src/main/java/leetcode/Searching/BinarySearch/SingleElementinaSortedArray540.java
+++ src/main/java/leetcode/Searching/BinarySearch/SingleElementinaSortedArray540.java
@@ -42,5 +42,42 @@ public class SingleElementinaSortedArray540 {
         System.out.println(Arrays.toString(arr));
     }
 
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }else if(nums.length == 1){
+            return nums[0];
+        }else if(nums[nums.length - 1] != nums[nums.length - 2]){
+            return nums[nums.length - 1];
+        }else if(nums[0] != nums[1]){
+            return nums[0];
+        }
+
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = (start + end)/2 ;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }else if(mid % 2 == 1){
+                if(nums[mid-1] == nums[mid]){
+                    start = mid+1;
+                }else{
+                    end = mid - 1 ;
+                }
+            }else {     //index is even
+                if(nums[mid] == nums[mid+1]){
+                    start = mid + 1;
+                }else{
+                    end = mid + 1;
+                }
+
+            }
+        }
+
+        return -1;
+    }
+
 
 }
