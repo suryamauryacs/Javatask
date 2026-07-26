2d379407ede7b9408cdcd2a259e39b8a18fa5901
surya maurya <suryamauryacs@gmail.com>
2026-07-26 23:46:37 +0530

    SortColor leet 75

---
+++ src/main/java/leetcode/Searching/BinarySearch/SortColors75.java
@@ -0,0 +1,35 @@
+package leetcode.Searching.BinarySearch;
+
+import java.util.Scanner;
+
+public class SortColors75 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+
+    public static void swap(int[] nums , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+    }
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            } else{
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+}
