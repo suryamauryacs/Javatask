3947fb1c5d7013a5814823934fdf61272ebade1e
surya maurya <suryamauryacs@gmail.com>
2026-07-26 23:57:16 +0530

    SortColor leet 75

--- src/main/java/leetcode/Searching/BinarySearch/SortColors75.java
+++ src/main/java/leetcode/Searching/BinarySearch/SortColors75.java
@@ -1,5 +1,30 @@
 package leetcode.Searching.BinarySearch;
 
+//Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+//
+//We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+//
+//You must solve this problem without using the library's sort function.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [2,0,2,1,1,0]
+//Output: [0,0,1,1,2,2]
+//Example 2:
+//
+//Input: nums = [2,0,1]
+//Output: [0,1,2]
+//
+//
+//Constraints:
+//
+//n == nums.length
+//1 <= n <= 300
+//nums[i] is either 0, 1, or 2.
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,9 +38,12 @@ public class SortColors75 {
                         input.substring(input.indexOf('[') + 1, input.indexOf(']'))
                                 .split(","))
                 .map(String::trim)
+                .filter(s -> !s.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(sortColors(arr)));
 
     }
 
@@ -26,7 +54,7 @@ public class SortColors75 {
         nums[j] = temp;
 
     }
-    public void sortColors(int[] nums) {
+    public static int[] sortColors(int[] nums) {
         int low = 0, mid = 0, high = nums.length-1;
 
         while(mid <= high){
@@ -42,5 +70,7 @@ public class SortColors75 {
                 high--;
             }
         }
+
+        return nums;
     }
 }
