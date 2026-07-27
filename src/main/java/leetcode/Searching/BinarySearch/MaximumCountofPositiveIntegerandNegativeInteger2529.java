b4c7ddfe0a11efc476bf75378b2be10949bd5e4f
surya maurya <suryamauryacs@gmail.com>
2026-07-28 03:08:08 +0530

    leetcode 2529

--- src/main/java/leetcode/Searching/BinarySearch/MaximumCountofPositiveIntegerandNegativeInteger2529.java
+++ src/main/java/leetcode/Searching/BinarySearch/MaximumCountofPositiveIntegerandNegativeInteger2529.java
@@ -37,9 +37,50 @@ public class MaximumCountofPositiveIntegerandNegativeInteger2529 {
         int[] arr = Arrays.stream(input.substring(input.indexOf("[") + 1, input.indexOf("]")).split(","))
                 .filter(s -> !s.isEmpty()).map(String::trim).mapToInt(Integer::parseInt).toArray();
 
-
         System.out.println(Arrays.toString(arr));
+        System.out.println(maximumCount(arr));
+    }
 
+    public static int maximumCount(int[] nums) {
+        int lastnegIndex = lastNegative(nums) + 1;
+        int firstPosIndex = nums.length - firstPostIndex(nums);
+        return Math.max(lastnegIndex, firstPosIndex);
 
+    }
+
+    public static int firstPostIndex(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = nums.length;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] > 0){
+                ans = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int lastNegative(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+
+        int ans = -1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(nums[mid] < 0){
+                ans = mid;
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        return ans;
     }
 }
