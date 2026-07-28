8631db9721e3615d3f44af8f02b8806bc820dd43
surya maurya <suryamauryacs@gmail.com>
2026-07-29 02:24:51 +0530

    leetcode 1491

--- src/main/java/leetcode/array/Easy/part4/AverageSalaryExcludingtheMinimumandMaximumSalary1491.java
+++ src/main/java/leetcode/array/Easy/part4/AverageSalaryExcludingtheMinimumandMaximumSalary1491.java
@@ -41,6 +41,26 @@ public class AverageSalaryExcludingtheMinimumandMaximumSalary1491 {
         int[] salary = Arrays.stream(input.substring(input.indexOf("[") +1, input.indexOf("]")).split(","))
                 .filter(s -> !s.isEmpty()).map(String :: trim).mapToInt(Integer :: parseInt).toArray();
 
-        System.out.println();
+        System.out.println(average(salary));
+    }
+
+    public static double average(int[] salary) {
+        int max = salary[0];
+        int min = salary[0];
+        int sum = salary[0];
+
+        for(int i = 1;i<salary.length;i++){
+            if(max < salary[i]){
+                max = salary[i];
+            }
+
+            if(min > salary[i]){
+                min = salary[i];
+            }
+
+            sum += salary[i];
+        }
+
+        return (double) (sum - max - min) / (salary.length - 2);
     }
 }
