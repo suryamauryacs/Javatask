5c21941f3bd2f154a86d02b788784380955e8320
surya maurya <suryamauryacs@gmail.com>
2026-08-01 14:12:14 +0530

    Leet 316

--- src/main/java/leetcode/Stack/RemoveDuplicateLetters316.java
+++ src/main/java/leetcode/Stack/RemoveDuplicateLetters316.java
@@ -2,6 +2,7 @@ package leetcode.Stack;
 //https://leetcode.com/problems/remove-duplicate-letters/description/
 
 import java.util.Scanner;
+import java.util.Stack;
 
 
 //Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
@@ -27,5 +28,47 @@ public class RemoveDuplicateLetters316 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String; ");
+//        String s = sc.nextLine();
+//        s = s.substring(5, s.length()-1);
+        String s = sc.nextLine().substring(5).replace("\"", "");
+        System.out.println(s);
+
+        System.out.println(removeDuplicate(s));
+
     }
+
+    public static String removeDuplicate(String s){
+        int[] lastIndex = new int[26];
+
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            int idx = (int)(ch - 'a');
+            lastIndex[idx] = i;
+        }
+
+        boolean[] present = new boolean[26];
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            int idx = (int)(ch - 'a');
+            if(present[idx] == false){
+                while(st.size() > 0 && st.peek() > ch && lastIndex[(int)(st.peek()-'a')]> i){
+                    present[(int)(st.peek()-'a')] = false;
+                    st.pop();
+
+                }
+
+                st.push(ch);
+                present[idx] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        while(st.size() >  0){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+
+
 }
