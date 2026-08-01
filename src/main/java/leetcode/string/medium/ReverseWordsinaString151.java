eae83e8088c6f53f48297b93ba6d5de935a19e1e
surya maurya <suryamauryacs@gmail.com>
2026-08-01 14:22:39 +0530

    Leet 151[C

---
+++ src/main/java/leetcode/string/medium/ReverseWordsinaString151.java
@@ -0,0 +1,32 @@
+package leetcode.string.medium;
+
+import java.util.Scanner;
+//https://leetcode.com/problems/reverse-words-in-a-string/description/
+
+//Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "bcabc"
+//Output: "abc"
+//Example 2:
+//
+//Input: s = "cbacdcbc"
+//Output: "acdb"
+//
+//
+//Constraints:
+//
+//1 <= s.length <= 104
+//s consists of lowercase English letters.
+
+
+public class ReverseWordsinaString151 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
