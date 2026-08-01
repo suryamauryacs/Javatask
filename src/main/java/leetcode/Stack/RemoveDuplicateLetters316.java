60a48d8af7ae8189c79a472f963ba185a47943fd
surya maurya <suryamauryacs@gmail.com>
2026-08-01 13:35:39 +0530

    Leet 316

---
+++ src/main/java/leetcode/Stack/RemoveDuplicateLetters316.java
@@ -0,0 +1,31 @@
+package leetcode.Stack;
+//https://leetcode.com/problems/remove-duplicate-letters/description/
+
+import java.util.Scanner;
+
+
+//Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "bcabc"
+//Output: "abc"
+//Example 2:
+//
+//Input: s = "cbacdcbc"
+//Output: "acdb"
+//
+//
+//Constraints:
+//
+//1 <= s.length <= 104
+//s consists of lowercase English letters.
+
+public class RemoveDuplicateLetters316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String; ");
+    }
+}
