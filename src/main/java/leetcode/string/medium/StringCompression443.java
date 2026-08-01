f58cf4c4788f9671709bc3ef754796eaf5df3a78
surya maurya <suryamauryacs@gmail.com>
2026-08-01 17:23:17 +0530

    Leet 443

--- src/main/java/leetcode/string/medium/StringCompression443.java
+++ src/main/java/leetcode/string/medium/StringCompression443.java
@@ -62,5 +62,32 @@ public class StringCompression443 {
 
     }
 
+    public static  int Compress(char[] chars){
+        int count = 1;
+        StringBuilder sb = new StringBuilder("");
+        sb.append(chars[0]);
+
+        for(int i = 1;i<chars.length;i++){
+            if(chars[i-1] != chars[i]){
+                if(count > 1){
+                    sb.append(count+"");
+                }
+                sb.append(chars[i]);
+                count = 1;
+            }
+            else{
+                count++;
+            }
+        }
+
+        if(count > 1){
+            sb.append(count+"");
+        }
+        for(int i = 0;i<sb.length();i++){
+            chars[i] = sb.charAt(i);
+
+        }
+        return sb.length();
+    }
 
 }
