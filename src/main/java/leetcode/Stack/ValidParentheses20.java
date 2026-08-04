de85a67246e9f3455a198a76a371c41d57835221
surya maurya <suryamauryacs@gmail.com>
2026-08-04 23:54:49 +0530

    Leet 20

--- src/main/java/leetcode/Stack/ValidParentheses20.java
+++ src/main/java/leetcode/Stack/ValidParentheses20.java
@@ -1,6 +1,7 @@
 package leetcode.Stack;
 
 import java.util.Scanner;
+import java.util.Stack;
 //
 
 
@@ -9,5 +10,33 @@ public class ValidParentheses20 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String : ");
         String s = sc.nextLine();
+
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '[' || ch == '{' || ch == '('){
+                st.push(ch);
+            }else if(st.size() > 0 && ch == ']' && st.peek() == '['){
+                st.pop();
+            }else if(st.size() > 0 && ch == '}' && st.peek() == '{'){
+                st.pop();
+            }else if(st.size() > 0 && ch == ')' && st.peek() == '('){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+
+        if(st.size() == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
