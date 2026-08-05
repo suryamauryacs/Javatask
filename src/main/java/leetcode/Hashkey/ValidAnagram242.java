b5511c2be2c57885908fc01005d1ad71a572ea8d
surya maurya <suryamauryacs@gmail.com>
2026-08-05 13:21:27 +0530

    Leet 242

---
+++ src/main/java/leetcode/Hashkey/ValidAnagram242.java
@@ -0,0 +1,11 @@
+package leetcode.Hashkey;
+
+import java.util.Scanner;
+//https://leetcode.com/problems/valid-anagram/description/
+
+public class ValidAnagram242 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
