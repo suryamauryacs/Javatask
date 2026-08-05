22097eb4a7f1a539d6eecd5d774b50a296f5cf8a
surya maurya <suryamauryacs@gmail.com>
2026-08-05 14:07:14 +0530

    Leet 242

--- src/main/java/leetcode/Hashkey/ValidAnagram242.java
+++ src/main/java/leetcode/Hashkey/ValidAnagram242.java
@@ -1,5 +1,6 @@
 package leetcode.Hashkey;
 
+import java.util.HashMap;
 import java.util.Scanner;
 //https://leetcode.com/problems/valid-anagram/description/
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
@@ -35,5 +36,38 @@ public class ValidAnagram242 {
         System.out.println("s = " + s);
         System.out.println("t = " + t);
 
+    }
+
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(hm.containsKey(ch) == true){
+                hm.put(ch, hm.get(ch) + 1);
+            }else{
+                hm.put(ch, 1);
+            }
+        }
+
+        for(int i = 0;i<t.length();i++){
+            char ch = t.charAt(i);
+
+            if(hm.containsKey(ch) == true){
+                if(hm.get(ch) == 1){
+                    hm.remove(ch);
+                }else{
+                    hm.put(ch, hm.get(ch) - 1);
+                }
+            }else{
+                return false;
+            }
+        }
+        if(hm.size() == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
