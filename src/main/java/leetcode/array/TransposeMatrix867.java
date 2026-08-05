ffff4b765e5993409ab1d3d589ba02a6f396f757
surya maurya <suryamauryacs@gmail.com>
2026-08-05 23:16:06 +0530

    Leet 867

--- src/main/java/leetcode/array/TransposeMatrix867.java
+++ src/main/java/leetcode/array/TransposeMatrix867.java
@@ -1,5 +1,6 @@
 package leetcode.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 //Given a 2D integer array matrix, return the transpose of matrix.
 //
@@ -23,6 +24,32 @@ public class TransposeMatrix867 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Array : ");
+        String str = sc.nextLine();
+
+                // Extract matrix part
+                String matrixPart = str.substring(str.indexOf("[["));
+
+                // Remove outer brackets
+                matrixPart = matrixPart.substring(2, matrixPart.length() - 2);
+
+                // Split rows
+                String[] rows = matrixPart.split("\\],\\[");
+
+                int[][] matrix = new int[rows.length][];
+
+                for (int i = 0; i < rows.length; i++) {
+                    String[] nums = rows[i].split(",");
+                    matrix[i] = new int[nums.length];
+
+                    for (int j = 0; j < nums.length; j++) {
+                        matrix[i][j] = Integer.parseInt(nums[j]);
+                    }
+                }
+
+                // Print matrix
+                for (int[] row : matrix) {
+                    System.out.println(Arrays.toString(row));
+                }
 
     }
 }
