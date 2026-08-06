fae464a395fc926fbefc30818d06456abb499bfc
surya maurya <suryamauryacs@gmail.com>
2026-08-07 03:17:56 +0530

    Leet 74

---
+++ src/main/java/leetcode/array/matrix/Searcha2DMatrix74.java
@@ -0,0 +1,11 @@
+package leetcode.array.matrix;
+//https://leetcode.com/problems/search-a-2d-matrix/description/
+
+
+public class Searcha2DMatrix74 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
