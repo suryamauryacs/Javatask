6eae764753c34b1f7f92e465d0332dd9d293d07b
surya maurya <suryamauryacs@gmail.com>
2026-08-06 14:24:03 +0530

    Leet 59

--- src/main/java/leetcode/array/matrix/SpiralMatrixII59.java
+++ src/main/java/leetcode/array/matrix/SpiralMatrixII59.java
@@ -1,5 +1,7 @@
 package leetcode.array.matrix;
 
+//https://leetcode.com/problems/spiral-matrix-ii/description/
+
 
 //Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
 //
