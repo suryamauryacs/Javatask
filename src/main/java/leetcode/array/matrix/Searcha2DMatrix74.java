b5bdf37a4000db534253862a302d2502a4083b0d
surya maurya <suryamauryacs@gmail.com>
2026-08-07 03:19:16 +0530

    Leet 74

--- src/main/java/leetcode/array/matrix/Searcha2DMatrix74.java
+++ src/main/java/leetcode/array/matrix/Searcha2DMatrix74.java
@@ -2,9 +2,39 @@ package leetcode.array.matrix;
 //https://leetcode.com/problems/search-a-2d-matrix/description/
 
 
+import java.util.Scanner;
+
 public class Searcha2DMatrix74 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Array of Matrix: ");
+        String input = sc.nextLine();
 
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int value = matrix[mid / n][mid % n];
+
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
     }
 
 
