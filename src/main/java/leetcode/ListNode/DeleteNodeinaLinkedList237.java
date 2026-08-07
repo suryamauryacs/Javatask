01f51bb44138096a4a31459193befa32fd3ef1ec
surya maurya <suryamauryacs@gmail.com>
2026-08-08 04:06:41 +0530

    Leet 237

---
+++ src/main/java/leetcode/ListNode/DeleteNodeinaLinkedList237.java
@@ -0,0 +1,22 @@
+package leetcode.ListNode;
+
+public class DeleteNodeinaLinkedList237 {
+    static class NodeList{
+        int val;
+        NodeList next;
+
+        NodeList(){}
+
+        NodeList(int val){
+            this.val = val;
+        }
+
+        NodeList(int val, NodeList next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+    static void main() {
+
+    }
+}
