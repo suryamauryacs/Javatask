4c52133335e8be048a1d0e2475fc95b949d3dec6
surya maurya <suryamauryacs@gmail.com>
2026-08-07 18:06:01 +0530

    Leet 83

--- src/main/java/leetcode/ListNode/RemoveDuplicatesfromSortedList83.java
+++ src/main/java/leetcode/ListNode/RemoveDuplicatesfromSortedList83.java
@@ -1,85 +1,106 @@
 package leetcode.ListNode;
 
 import java.util.Scanner;
+//Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: head = [1,1,2]
+//Output: [1,2]
+//Example 2:
+//
+//
+//Input: head = [1,1,2,3,3]
+//Output: [1,2,3]
+//
+//
+//Constraints:
+//
+//The number of nodes in the list is in the range [0, 300].
+//-100 <= Node.val <= 100
+//The list is guaranteed to be sorted in ascending order.
 
 
-
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {}
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 
 public class RemoveDuplicatesfromSortedList83 {
-    public static void main() {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the Node of Array : ");
         String input = sc.nextLine();
-//                String input = "head = [1,1,2,3,3]";
 
         String headStr = input.replace("head = ", "");
+
         ListNode head = buildList(headStr);
+
         ListNode result = deleteDuplicates(head);
+
         printList(result);
-        }
+
+        sc.close();
+    }
 
     static ListNode buildList(String str) {
         str = str.substring(1, str.length() - 1);
-        if(str.length() == 0)
+
+        if (str.length() == 0) {
             return null;
+        }
+
         String[] nums = str.split(",");
+
         ListNode dummy = new ListNode(-1);
         ListNode curr = dummy;
-        for(String num : nums) {
-            curr.next = new ListNode(Integer.parseInt(num));
+
+        for (String num : nums) {
+            curr.next = new ListNode(Integer.parseInt(num.trim()));
             curr = curr.next;
         }
+
         return dummy.next;
     }
 
-
     static void printList(ListNode head) {
-
-        while(head != null) {
+        while (head != null) {
             System.out.print(head.val + " ");
             head = head.next;
         }
     }
-}
 
+    public static ListNode deleteDuplicates(ListNode head) {
 
+        ListNode current = head;
 
-public static ListNode deleteDuplicates(ListNode head) {
-    if(head == null){
-        return head;
-    }
-    ListNode dummy = new ListNode(head.val);
-    ListNode ans = dummy;
-    ListNode ptr = head.next;
+        while (current != null && current.next != null) {
 
-    while(ptr != null){
-        if(ptr.val != dummy.val){
-            ListNode temp = new ListNode(ptr.val);
-            dummy.next = temp;
-            dummy = dummy.next;
+            if (current.val == current.next.val) {
+                current.next = current.next.next; // remove duplicate
+            } else {
+                current = current.next;
+            }
         }
 
-        ptr = ptr.next;
+        return head;
     }
-
-    return ans;
 }
-
-
