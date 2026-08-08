46a3dfcb72ff29bcc1ef1004345b4643af915cf6
surya maurya <suryamauryacs@gmail.com>
2026-08-08 18:30:19 +0530

    Leet 2816

--- src/main/java/leetcode/ListNode/DoubleaNumberRepresentedasaLinkedList2816.java
+++ src/main/java/leetcode/ListNode/DoubleaNumberRepresentedasaLinkedList2816.java
@@ -53,7 +53,8 @@ public class DoubleaNumberRepresentedasaLinkedList2816 {
 
         ListNode head = buildList(str);
 
-        doubleIt(head);
+        ListNode result =doubleIt(head);
+        printList(result);
 
     }
 
@@ -76,12 +77,54 @@ public class DoubleaNumberRepresentedasaLinkedList2816 {
 
     static void printList(ListNode head){
         while(head != null){
-            System.out.println(head.val +" ");
+            System.out.print(head.val +" ");
             head = head.next;
         }
     }
 
-    public ListNode doubleIt(ListNode head){
+    public static ListNode reverseLL(ListNode head){
+        ListNode curr = head;
+        ListNode pre = null;
 
+        while (curr != null){
+            ListNode temp = curr.next;
+            curr.next = pre;
+
+            pre = curr;
+            curr = temp;
+        }
+
+        return pre;
+    }
+    public static ListNode doubleIt(ListNode head){
+        ListNode nhead = reverseLL(head);
+        ListNode dummy = new ListNode(-1);
+
+        ListNode ans = dummy;
+
+        int carry = 0;
+
+        while(nhead != null){
+            int sum = nhead.val + nhead.val + carry;
+            int d = sum % 10;
+            carry = sum / 10;
+
+            ListNode temp = new ListNode(d);
+            dummy.next = temp;
+            dummy = dummy.next;
+
+            nhead = nhead.next;
+
+        }
+
+        if(carry > 0){
+            ListNode temp = new ListNode(carry);
+            dummy.next = temp;
+
+
+        }
+
+        ans = reverseLL(ans.next);
+        return ans;
     }
 }
